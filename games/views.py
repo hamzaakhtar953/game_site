49511a62ce5df8e56b631027a368544614764b4b
hamzaakhtar <hamzaakhtar0@gmail.com>
2019-08-05 08:36:37 +0500

    Added favourites attribute and business logic

--- games/views.py
+++ games/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Genre
+from .models import Genre, Game
 
 
 def index(request):
@@ -12,3 +13,18 @@ def index(request):
 def detail(request, genre_id):
     genre = get_object_or_404(Genre, pk=genre_id)
     return render(request, 'games/detail.html', {'genre': genre})
+
+
+def favourite(request, genre_id):
+    genre = get_object_or_404(Genre, pk=genre_id)
+    try:
+        game = genre.game_set.get(pk=request.POST['game'])
+    except (KeyError, Game.DoesNotExist):
+        return render(request, 'games/detail.html', {
+            'genre': genre,
+            'error_message': 'Incorrect game selected'
+        })
+    else:
+        game.is_favourite = True
+        game.save()
+        return HttpResponseRedirect(reverse('game:detail', args=(genre.id,)))
